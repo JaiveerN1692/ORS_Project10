3de1678849fe2cfdbc5ed34e8da9899e81a62db5
JaiveerN1692 <99002911+JaiveerN1692@users.noreply.github.com>
2023-04-18 13:40:52 +0530

    uploaded

---
+++ projectORS/src/main/java/com/rays/dao/FacultyDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.FacultyDTO;
+/**
+ * @author Jaiveer Nahta
+ *
+ */
+public interface FacultyDAOInt extends BaseDAOInt<FacultyDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/RoleDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.RoleDTO;
+
+/**
+ * Role DAO interface.
+ * @author Jaiveer Nahta
+ */
+public interface RoleDAOInt extends BaseDAOInt<RoleDTO> {
+
+}
